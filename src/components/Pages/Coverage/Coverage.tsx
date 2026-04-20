c6f3528788c08c65d6046dd6c9efdcb9910c9e38
Shoaib Taimur <mitashoaib@gmail.com>
2026-04-21 01:17:29 +0600

    Add search functionality to Coverage component and create reusable Input component

--- src/components/Pages/Coverage/Coverage.tsx
+++ src/components/Pages/Coverage/Coverage.tsx
@@ -1,7 +1,10 @@
 import type { LatLngExpression } from "leaflet";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Search } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
 
 type center = {
   region: string;
@@ -16,6 +19,7 @@ type center = {
 
 const Coverage = () => {
   const [centers, setCenters] = useState<center[]>([]);
+  const mapRef=useRef(null);
 
   useEffect(() => {
     const loadData = async () => {
@@ -26,27 +30,52 @@ const Coverage = () => {
     loadData();
   }, []);
 
+  const handleSearch=(e: React.SyntheticEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    const formData=new FormData(e.currentTarget);
+    const location=formData.get("location") as string;
+    const district=centers.find(c=>c.district.toLowerCase().includes(location.toLowerCase()));
+    if(district){
+      const coord=[district.latitude,district.longitude];
+      console.log(district, coord);
+      mapRef.current.flyTo(coord,14);
+    }
+
+  }
+
   const position: LatLngExpression = [23.685, 90.3563];
   return (
     <div className="bg-white px-8 md:px-20 py-16 rounded-2xl my-8">
       <h1 className="text-[#03373D] text-[20px] md:text-[35px] font-extrabold">
         We are available in 64 districts
       </h1>
-      <div></div>
-      <h1 className="text-[#03373D] text-[15px] md:text-[20px] font-extrabold mt-10">We deliver almost all over Bangladesh</h1>
-      <div className="w-full h-200 mt-5">
+      <form onSubmit={handleSearch} className="mt-3">
+        <Search className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+        <Input
+          className="bg-background pl-9"
+          id="search-input"
+          placeholder="Search..."
+          type="search"
+          name="location"
+        />
+      </form>
+      <h1 className="text-[#03373D] text-[15px] md:text-[20px] font-extrabold mt-10">
+        We deliver almost all over Bangladesh
+      </h1>
+      <div className="w-full h-80 sm:h-100 mt-5">
         <MapContainer
-          className="h-200"
+          className="h-80 sm:h-100"
           center={position}
           zoom={13}
           scrollWheelZoom={false}
+          ref={mapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {centers.map((center,i) => (
-            <Marker key={i} position={[center.latitude,center.longitude]}>
+          {centers.map((center, i) => (
+            <Marker key={i} position={[center.latitude, center.longitude]}>
               <Popup>{center.district}</Popup>
             </Marker>
           ))}
