9ec61bb3200c6103eb5da34f38e9630a7fe1d77b
Shoaib Taimur <mitashoaib@gmail.com>
2026-04-23 14:09:36 +0600

    feat: update Order component to fetch and display parcel count from API

--- src/components/Pages/SendParcel-order/Order.tsx
+++ src/components/Pages/SendParcel-order/Order.tsx
@@ -1,8 +1,24 @@
+import { AuthContext } from "@/Context/AuthContext";
+import AxiosSecure from "@/Hooks/AxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
 
 const Order = () => {
+    const info=useContext(AuthContext);
+    const user=info?.user;
+
+    const axiosSecure=AxiosSecure();
+
+    const {data:parcels=[]}=useQuery({
+        queryKey:["allParcels",user?.email],
+        queryFn:async()=>{
+            const res=await axiosSecure.get("/parcels");
+            return res.data;
+        }
+    })
     return (
         <div className="bg-white rounded-2xl px-8 md:px-10 py-6">
-            order
+            order {parcels.length}
         </div>
     );
 };
