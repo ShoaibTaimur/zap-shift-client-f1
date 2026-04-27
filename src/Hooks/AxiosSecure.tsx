64b92db961294597b819c80ba913dbd63170ca41
Shoaib Taimur <mitashoaib@gmail.com>
2026-04-28 01:38:57 +0600

    fix: update baseURL in AxiosSecure to use production server

--- src/Hooks/AxiosSecure.tsx
+++ src/Hooks/AxiosSecure.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure=axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:"https://zap-shift-server-f1.vercel.app/"
 })
 const AxiosSecure = () => {
     return axiosSecure;
